34dc2d9d6740dedae2ec62f2303cac1e92286903
logan <1940124602@qq.com>
2022-12-31 12:38:29 +0800

    second commit

--- src/main/java/test.java
+++ src/main/java/test.java
@@ -2,5 +2,6 @@ public class test {
     public static void main(String[] args) {
         System.out.println("我现在正在用idea操作Git");
         System.out.println("我目前还不会上传一个项目到GitHub");
+        System.out.println("我刚刚提交了一个到本地库");
     }
 }
